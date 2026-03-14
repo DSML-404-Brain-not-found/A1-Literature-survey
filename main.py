66bf42da296ed0d0156b24d4e1f5fb394f9d1011
Chia-Chenn <cclee@g.ncu.edu.tw>
2026-03-14 17:08:50 +0800

    fix: close annotation

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ RANDOM_SEED = 8
 # ----------------------------------------------------------
 # Data-level Methods
 # ----------------------------------------------------------
-# if __name__ == "__main__":
-#     file_path = r"D:\404-Brain-not-found"
-#     csv_name = "result"
-#     method = "-"
-#     yeast_num = "1"
-#     # yeast_num = "4"
-#     # yeast_num = "6"
+if __name__ == "__main__":
+    file_path = r"D:\404-Brain-not-found"
+    csv_name = "result"
+    method = "-"
+    yeast_num = "1"
+    # yeast_num = "4"
+    # yeast_num = "6"
     
-#     # 使用以下變數的 True / False 來決定要執行哪些模型
-#     run_experiment(run_knn_model=True, run_rf_model=True, run_svm_model=True, file_path=file_path,csv_name=csv_name,method=method,yeast=yeast_num)
+    # 使用以下變數的 True / False 來決定要執行哪些模型
+    run_experiment(run_knn_model=True, run_rf_model=True, run_svm_model=True, file_path=file_path,csv_name=csv_name,method=method,yeast=yeast_num)
 
 
 # ----------------------------------------------------------
